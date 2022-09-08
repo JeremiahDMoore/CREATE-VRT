0e2f4d4cb599265aecb1fe95391b4102f878765d
stacksantos <stacksantos@proton.me>
2022-09-08 13:36:17 -0500

    add files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* -------------------------------------------------------------------------- */
+/*                              IMPORT STUFF HERE                             */
+/* -------------------------------------------------------------------------- */
+
+
+
+function App() {
+
+	return (
+    <div>
+      {/* DELETE THIS AND BUILD YOUR APP */}
+      {/* EVERYTHING MUST BE WRAPPED IN A CONTAINING DIV */}
+      CHANGE THIS TO SEE IF EVERYTHING IS WORKING
+		</div>
+	);
+}
+
+export default App;
